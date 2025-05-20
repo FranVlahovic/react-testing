8489b42207560e86840e883fa5c10c20a4823a38
FranVlahovic <franvlahovic@proton.me>
2025-05-20 22:55:13 +0200

    feat: Add test files & update configs for them

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,22 @@
-export default function App(){
-    return ();
+// First Query Test //
+// const App = () => <h1>Our First Test</h1>;
+// export default App;
+
+//Simulating user events //
+import { useState } from "react";
+
+const App =() => {
+    const [heading, setHeading] = useState("Magnificent Monkeys");
+
+    const clickHandler =() => {
+        setHeading("Radical Rhinos");
+    };
+
+    return (
+        <>
+            <button type="button" onClick={clickHandler}>Click Me</button>
+            <h1>{heading}</h1>
+        </>
+    )
 }
+export default App;
